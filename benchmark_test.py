cf58ed8d318d64668bfa767981ec176daa9ee2bf
Joe Le-Ba <leba@google.com>
2020-02-11 11:40:03 +0100

    only classify when there's a platform

--- benchmark_test.py
+++ benchmark_test.py
@@ -93,8 +93,8 @@ class BenchmarkFunctionTests(absltest.TestCase):
   def test_build_bazel_binary_exists(self, unused_chdir_mock,
                                      unused_exists_mock):
     with mock.patch.object(sys, 'stderr', new=mock_stdio_type()) as mock_stderr:
-      benchmark._build_bazel_binary('commit', 'repo_path', 'outroot', 'platform')
-    self.assertEqual('Binary exists at outroot/platform/commit/bazel, reusing...',
+      benchmark._build_bazel_binary('commit', 'repo_path', 'outroot')
+    self.assertEqual('Binary exists at outroot/commit/bazel, reusing...',
                      mock_stderr.getvalue())
 
   @mock.patch.object(benchmark.os.path, 'exists', return_value=False)
@@ -108,15 +108,15 @@ class BenchmarkFunctionTests(absltest.TestCase):
     with mock.patch.object(sys, 'stderr', new=mock_stdio_type()) as mock_stderr, \
       mock.patch('benchmark.git.Repo') as mock_repo_class:
       mock_repo = mock_repo_class.return_value
-      benchmark._build_bazel_binary('commit', mock_repo, 'outroot', 'platform')
+      benchmark._build_bazel_binary('commit', mock_repo, 'outroot')
 
     mock_repo.git.checkout.assert_called_once_with('-f', 'commit')
     self.assertEqual(
         ''.join([
             'Building Bazel binary at commit commit',
             'bazel build //src:bazel',
-            'Copying bazel binary to outroot/platform/commit/bazel',
-            'chmod +x outroot/platform/commit/bazel'
+            'Copying bazel binary to outroot/commit/bazel',
+            'chmod +x outroot/commit/bazel'
         ]), mock_stderr.getvalue())
 
   def test_single_run(self):
